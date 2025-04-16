0ae77eeac25d6b084c20fd47be5f03f6101314b4
igorocky <123@gmail.com>
2025-04-16 12:07:51 +0200

    NotesCmp.processClickedNote().

--- music/src/main/java/org/igye/sandbox/music/notes/NotesCmp.java
+++ music/src/main/java/org/igye/sandbox/music/notes/NotesCmp.java
@@ -1,5 +1,6 @@
 package org.igye.sandbox.music.notes;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.igye.sandbox.music.notes.impl.KeyboardCmpImpl;
 
 import javax.swing.JPanel;
@@ -11,25 +12,62 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.EnumMap;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 class NotesCmp extends JPanel {
     private final NoteUtils noteUtils;
     private final KeyboardCmp keyboardCmp;
     private final List<Integer> clickedNotes = new ArrayList<>();
 
+    private final List<Pair<Clef, Integer>> notesToRepeat;
+    private boolean lastClickedNoteIsCorrect = true;
+    private final Map<Clef, Map<Integer, Integer>> noteCounts;
+    private final Map<Clef, Map<Integer, Integer>> noteTotalCounts;
+
     public NotesCmp(NoteUtils noteUtils) {
         this.noteUtils = noteUtils;
         keyboardCmp = new KeyboardCmpImpl(noteUtils);
+
+        noteCounts = new EnumMap<>(Clef.class);
+        noteCounts.put(Clef.BASS, new HashMap<>());
+        noteCounts.put(Clef.TREBLE, new HashMap<>());
+
+        noteTotalCounts = new EnumMap<>(Clef.class);
+        noteTotalCounts.put(Clef.BASS, new HashMap<>());
+        noteTotalCounts.put(Clef.TREBLE, new HashMap<>());
+
+        notesToRepeat = new ArrayList<>();
+        int bassFirstNote = noteUtils.strToNote("1D");
+        int bassLastNote = noteUtils.strToNote("5D");
+        int trebleFirstNote = noteUtils.strToNote("2B");
+        int trebleLastNote = noteUtils.strToNote("6B");
+        for (int i = bassFirstNote; i <= bassLastNote; i++) {
+            if (noteUtils.isWhiteKey(i)) {
+                notesToRepeat.add(Pair.of(Clef.BASS, i));
+                noteCounts.get(Clef.BASS).put(i, 0);
+                noteTotalCounts.get(Clef.BASS).put(i, 0);
+            }
+        }
+        for (int i = trebleFirstNote; i <= trebleLastNote; i++) {
+            if (noteUtils.isWhiteKey(i)) {
+                notesToRepeat.add(Pair.of(Clef.TREBLE, i));
+                noteCounts.get(Clef.TREBLE).put(i, 0);
+                noteTotalCounts.get(Clef.TREBLE).put(i, 0);
+            }
+        }
+        Collections.shuffle(notesToRepeat);
+
 
         addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
                 Integer clickedNote = keyboardCmp.getClickedNote(e.getX(), e.getY());
                 if (clickedNote != null) {
-                    clickedNotes.add(clickedNote);
+                    processClickedNote(clickedNote);
                 }
-                repaint();
             }
         });
     }
@@ -45,13 +83,76 @@ class NotesCmp extends JPanel {
         g.fillRect(0, 0, (int) windowWidth, (int) windowHeight);
 
         Graphics2D g2 = (Graphics2D) g;
-        renderClickedNotes(g2);
+        renderCurNote(g2);
 
         double keyboardWidth = windowWidth * 0.9;
         double keyboardHeight = keyboardWidth * (15.0 / 122.5);
         Rect keyboardRect = Rect.make(0, 0, keyboardWidth, keyboardHeight);
         keyboardRect.setMidMid(Point.make(windowWidth / 2, windowHeight / 2));
         keyboardCmp.render(g2, keyboardRect);
+    }
+
+    private void processClickedNote(int clickedNote) {
+        Pair<Clef, Integer> curClefAndNote = notesToRepeat.get(0);
+        Clef curClef = curClefAndNote.getLeft();
+        int curNote = curClefAndNote.getRight();
+        if (curNote != clickedNote) {
+            lastClickedNoteIsCorrect = false;
+            resetCount(noteCounts, curClef, curNote);
+        } else {
+            lastClickedNoteIsCorrect = true;
+            incCount(noteCounts, curClef, curNote);
+            incCount(noteTotalCounts, curClef, curNote);
+            notesToRepeat.removeFirst();
+            int correctAnsCnt = getCount(noteTotalCounts, curClef, curNote);
+            if (correctAnsCnt == 1) {
+                notesToRepeat.add(6, curClefAndNote);
+            } else if (correctAnsCnt == 2) {
+                notesToRepeat.add(19, curClefAndNote);
+            } else {
+                notesToRepeat.add(curClefAndNote);
+            }
+        }
+        repaint();
+        printTotalCounts();
+    }
+
+    private int getCount(Map<Clef, Map<Integer, Integer>> counts, Clef clef, int note) {
+        return counts.get(clef).get(note);
+    }
+
+    private void resetCount(Map<Clef, Map<Integer, Integer>> counts, Clef clef, int note) {
+        counts.get(clef).put(note, 0);
+    }
+
+    private void incCount(Map<Clef, Map<Integer, Integer>> counts, Clef clef, int note) {
+        Map<Integer, Integer> subCounts = counts.get(clef);
+        subCounts.put(note, subCounts.get(note) + 1);
+    }
+
+    private void printTotalCounts() {
+        System.out.println("------------------------------------------------------------------------------");
+        printCounts("Total counts", noteTotalCounts);
+        System.out.println("------------------------------------------------------------------------------");
+
+    }
+
+    private void printCounts(String title, Map<Clef, Map<Integer, Integer>> counts) {
+        System.out.println(title + ":");
+        printCountsForClef(Clef.BASS, counts.get(Clef.BASS));
+        printCountsForClef(Clef.TREBLE, counts.get(Clef.TREBLE));
+
+    }
+
+    private void printCountsForClef(Clef clef, Map<Integer, Integer> counts) {
+        System.out.println("    " + clef + ":");
+        counts.entrySet().stream()
+            .sorted(Map.Entry.<Integer, Integer>comparingByValue().reversed())
+            .forEach(entry ->
+                System.out.println(String.format(
+                    "        %s: %s", noteUtils.noteToStr(entry.getKey()), entry.getValue()
+                ))
+            );
     }
 
     private void renderClickedNotes(Graphics2D g) {
@@ -62,5 +163,21 @@ class NotesCmp extends JPanel {
             .toList();
         g.setColor(Color.BLACK);
         noteUtils.renderStaff(g, rect, Clef.TREBLE, notesToRender);
+    }
+
+    private void renderCurNote(Graphics2D g) {
+        Pair<Clef, Integer> curClefAndNote = notesToRepeat.get(0);
+        Clef curClef = curClefAndNote.getLeft();
+        int curNote = curClefAndNote.getRight();
+        double scaleFactor = 1.5;
+        Rect rect = Rect.make(20, 200, 50 * scaleFactor, 40 * scaleFactor);
+        if (lastClickedNoteIsCorrect) {
+            g.setColor(Color.BLACK);
+        } else {
+            g.setColor(Color.RED);
+        }
+        noteUtils.renderStaff(
+            g, rect, curClef, List.of(List.of(noteUtils.intToNote(curNote, NoteAccidental.SHARP)))
+        );
     }
 }
